293ddd5997d28c751561273326adfd0b2ce21cc3
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2025-03-05 21:36:07 +0700

    updates

--- interfacesegregationprinciple/right/repository/repository.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-import "gosolidprinciple/interfacesegregationprinciple/right/entity"
-
-// WorkRepository → Hanya menangani penyimpanan pekerja
-type WorkRepository interface {
-	SaveWorkRecord(worker entity.Workable) error
-}
